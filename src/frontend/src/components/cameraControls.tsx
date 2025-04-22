c215a53624adcb77a5b40d2877397347e9566f31
Kendrick Jackson <kjackson2199@outlook.com>
2025-04-22 15:21:37 -0500

    Add CameraControl component with recording functionality

---
+++ src/frontend/src/components/cameraControls.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ControlButton from "./button";
+
+const CameraControl: React.FC = () => {
+    const [isRecording, setIsRecording] = React.useState(false);
+    const [isTakingPhoto, setIsTakingPhoto] = React.useState(false);
+
+    const handleRecordClick = async () => {
+        try{
+            const response = await fetch("http://100.100.111.72:5333/record", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ record: !isRecording }),
+            });
+
+            if (response.ok) {
+                throw new Error("Failed to start recording");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    return(
+        <div>
+            <ControlButton useImage={false} imageUri="" onClick={handleRecordClick} />
+        </div>
+    );
+};
+
+export default CameraControl;
