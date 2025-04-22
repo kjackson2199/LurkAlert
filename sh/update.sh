170a7d987c1daa9786227d5dd52c3cd4433fe327
Kendrick Jackson <kjackson2199@outlook.com>
2025-04-22 10:34:23 -0500

    Add update and variables scripts for LurkAlert setup

---
+++ sh/update.sh
@@ -0,0 +1,37 @@
+source variables.sh
+# update script for LurkAlert
+# This script is intended to be run as root
+# check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+cd $USER_HOME
+
+# Check if zip file exists
+if [ -f "master.zip" ]; then
+    echo "master.zip already exists, deleting..."
+    rm -rfv master.zip
+fi
+
+# get latest repository on github
+wget $GITHUB_URL
+if [ $? -ne 0 ]; then
+    echo "Failed to download the latest version from GitHub"
+    exit 1
+fi
+
+unzip master.zip
+rm -rfv master.zip
+
+# Remove old directory if it exists
+if [ -d "LurkAlert" ]; then
+    echo "LurkAlert directory already exists, deleting..."
+    rm -rfv LurkAlert
+fi
+
+mv LurkAlert-master LurkAlert
+cd LurkAlert
+
+. $PYTHON_ENV_DIR/bin/activate
---
+++ sh/variables.sh
@@ -0,0 +1,9 @@
+USER_HOME="/home/$(logname)"
+
+SHELL_SCRIPT_DIR="$USER_HOME/LurkAlert/sh"
+REAPER_SERVICE_PATH="/etc/systemd/system/LurkAlert.service"
+SERVICE_NAME="LurkAlert"
+LURK_ALERT_ENV_FILE_PATH="/etc/profile.d/lurk_env.env"
+PYTHON_ENV_DIR="$USER_HOME/lurkenv"
+LURK_RECORDINGS_DIR="$USER_HOME/recordings"
+GITHUB_URL="https://github.com/kjackson2199/LurkAlert/archive/refs/heads/master.zip
