59483068de94f67eabe294650f18e8f2074ec627
Kendrick Jackson <kjackson2199@outlook.com>
2025-04-22 15:22:34 -0500

    Replace ControlButton components with CameraControl in App component

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import FeedViewer from "./components/feedViewer";
 import ControlButton from "./components/button";
+import CameraControl from "./components/cameraControls";
 
 const App: React.FC = () => {
   return(
     <div>
       <h1>Lurk Alert</h1>
       <FeedViewer />
-      <ControlButton useImage={false} imageUri="" onClick={() => console.log("Button clicked")} />
-      <ControlButton useImage={false} imageUri="" onClick={() => console.log("Button clicked")} />
-      <ControlButton useImage={false} imageUri="" onClick={() => console.log("Button clicked")} />
+      <CameraControl />
     </div>
   );
 }
